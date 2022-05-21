cd8889b1ed7af4636cd7b006d5c59e041329ac43
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-05-21 20:44:01 +0530

    created Blockchain()

--- main.js
+++ main.js
@@ -10,6 +10,34 @@ class Block{
         this.hash = this.calculateHash();
     }
     calculateHash() {
-        return SHA256(this.index,this.previousHash, this.timestamp, JSON.Stringify(this.data)).toString();
+        return SHA256(this.index,this.previousHash, this.timestamp, JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()]
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '21-05-2022', 'GenesisBlock: the first block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+
+let demoCoin = new Blockchain();
+demoCoin.addBlock(new Block(1, '20-05-2022', {amount: 4}))
+demoCoin.addBlock(new Block(2, '21-05-2022', { amount: 7 }))
+
+
+console.log(JSON.stringify(demoCoin, null, 4));
